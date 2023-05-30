1f9c13a6df60c1496f19869e2aa748aaca79ff8d
Calleb Camargo <113070078+Calleb21@users.noreply.github.com>
2023-05-30 10:33:00 -0300

    atv-mod-09]

---
+++ tarefa-ebac-mod09/src/br/com/calleb/Conversao.java
@@ -0,0 +1,13 @@
+package br.com.calleb;
+
+public class Conversao {
+
+    public static void main(String[] args) {
+
+        int num1 = 468324;
+        System.out.println("O número é: " + num1 + ".");
+
+        Integer num2 = num1;
+        System.out.println("O número é: " + num2 + ".");
+    }
+}
